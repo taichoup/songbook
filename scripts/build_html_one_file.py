ac8d502ee2b1fbfec9793f62ec6d593ee49b0c9a
taichoup <taichoup@gmx.net>
2018-01-28 22:18:54 +0100

    fix loop bug

--- scripts/build_html_one_file.py
+++ scripts/build_html_one_file.py
@@ -1,10 +1,10 @@
-import os, codecs
+import os, codecs, subprocess, time
 from bs4 import BeautifulSoup
-
 
 input_directory = "E:/Docs/GitHub/songbook/songs"
 output_directory = "E:/Docs/GitHub/songbook/songs_html_unique"
 ebook_title = "Manu's Songbook"
+kindlegen_dir = "C:\Users\pc\Downloads\kindlegen_win32_v2_9"
 
 soup_string = '<!doctype html><html lang="en"><head><meta charset="utf-8" /> <title>%s</title><link rel="stylesheet" href="style.css"  type="text/css" /></head><body><div id="toc"><h2>Manu&apos;s Songbook<br /></h2><ul></ul></div><div class="pagebreak"></div></body>' % ebook_title
 
@@ -52,12 +52,17 @@ for filename in os.listdir(input_directory):
             toc_entry.append(toc_entry_link)
             toc_entry_link.string = chapter_title
 
-
-        # Write all of this to a unique html file
-        outfile = codecs.open('../songs_html_unique/export.html', 'w+', encoding="utf-8")
-        outfile.write(soup.prettify())
-        outfile.close()
-
     except Exception as e:
         print e
         continue
+
+# Write all of this to a unique html file
+timestr = time.strftime("%Y%m%d-%H%M%S")
+outfile_name = "export_%s.html" % timestr
+outfile = codecs.open('../songs_html_unique/%s' % outfile_name, 'w+', encoding="utf-8")
+outfile.write(soup.prettify())
+outfile.close()
+
+if kindlegen_dir:
+    print "Building .mobi file..."
+    subprocess.call([r'%s\kindlegen.exe' % kindlegen_dir, '%s/%s' % (output_directory, outfile_name)])
